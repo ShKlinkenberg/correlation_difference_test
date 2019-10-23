681ea9a17873f5c36bddaae701fc7a74c5325046
Sharon <s.klinkenberg@uva.nl>
2019-10-23 12:14:40 +0200

    calculations added

--- functions.R
+++ functions.R
@@ -24,5 +24,15 @@ ComparingIndependentCorrelation <- function(r, n) {
   # Returns:
   #   The significance of the correlation
   
+  r.abs = abs(r)
   
+  z = FisherZTransformation(r.abs)
+  
+  # Zobserved = (z1 – z2) / (square root of [ (1 / N1 – 3) + (1 / N2 – 3) ]
+  
+  z.observed = (z[1] - z[2]) / sqrt( (1 / (n[1]-3)) + (1 / (n[2]-3)) )
+  
+  p.value = qnorm(z.observed)
+  
+  return(p.value)
 }
