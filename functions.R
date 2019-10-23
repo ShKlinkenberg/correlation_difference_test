cc39afdc7599cda70f25eb0380b13692e49bce10
Sharon <s.klinkenberg@uva.nl>
2019-10-23 11:45:33 +0200

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,5 +1,6 @@
 FisherZTransformation <- function(r) {
   # Transforms Pearson’s r to Fisher's Z
+  # Source: https://www.statisticshowto.datasciencecentral.com/fisher-z/
   #
   # Args:
   #   r: Pearson's correlation coefficient r
